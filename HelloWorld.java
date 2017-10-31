4e35c715335a5251a7ba0e3d5e171e9b47c2b8c8
Akee7740 <33219885+Akee7740@users.noreply.github.com>
2017-10-31 12:37:53 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
+      // Prints "Hello, World" in the terminal window
       System.out.println("Hello, World");
 	  System.out.println("AAKASH KADAM TIBCO");
    }
